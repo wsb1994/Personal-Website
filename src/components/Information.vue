e261b1637754b61cc6bb71c495f3b30f081fe93f
willofhouse <willofhouse@pop-os.localdomain>
2021-03-01 12:19:16 -0600

    added new components

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,9 +1,14 @@
 <template>
- <div class="flex-container">
-  <div class="Information">
-      <div class='pastel1'>
-      <img src="https://cutewallpaper.org/21/big-chungus-picture/Big-Chungus-that-you-can-print-and-color-it.-BigChungus.jpg"  style="width:200px;height:200px;">
-      <h1 class='pastel2'> Wungus </h1>
+
+    <div id="Information" >
+      <div v-for="info in projects" :key="info.language">
+        <div class="flex-container">
+        <div class="Information">
+          <div v-bind:class="info.language">
+            <img v-bind:src="info.img" style="width: 200px; height: 200px" />
+            <h1 class="pastel2">{{ info.language }} Professional</h1>
+          </div>
+        </div>
       </div>
   </div>
   </div>
@@ -12,26 +17,88 @@
 <script>
 export default {
   name: "Information",
+  projects: [],
+  data() {
+    return {
+      projects: [
+        {
+          img:
+            "https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png",
+          language: "Python",
+        },
+        {
+          img: "https://rustacean.net/assets/cuddlyferris.png",
+          language: "Rust",
+        },
+           {
+          img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png",
+          language: "Vue",
+        },
+           {
+          img: "https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png",
+          language: "MongoDB",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.pastel1 {
+.Python {
   color: black;
-  background: #ff9aa2;
+  background: #c3d2e5;
   font-family: "Georgia", "Times New Roman", Times, serif, serif;
   padding: 16px;
   border-style: solid;
+  border-radius: 30px;
+  width: 200px;
+}
+
+.Rust {
+  color: black;
+  background: #ffb7b2;
+  font-family: "Georgia", "Times New Roman", Times, serif, serif;
+  padding: 16px;
+  border-style: solid;
+  border-radius: 30px;
+  width: 200px;
+}
+.Vue {
+  color: black;
+  background:   #E2F0CB;
+  font-family: "Georgia", "Times New Roman", Times, serif, serif;
+  padding: 16px;
+  border-style: solid;
+  border-radius: 30px;
+  width: 200px;
+}
+
+.MongoDB {
+  color: black;
+  background: #B5EAD7;
+  font-family: "Georgia", "Times New Roman", Times, serif, serif;
+  padding: 16px;
+  border-style: solid;
+  border-radius: 30px;
+  width: 200px;
+}
+.pastel1 {
+  color: black;
+  background: #c3d2e5;
+  font-family: "Georgia", "Times New Roman", Times, serif, serif;
+  padding: 16px;
+  border-style: solid;
+  border-radius: 30px;
   width: 200px;
 }
 .pastel2 {
   color: black;
-  background: #ffb7b2;
   font-family: "Georgia", "Times New Roman", Times, serif, serif;
-  border-style: solid;
+  border-radius: 2px;
+  opacity: 20;
   width: 195px;
-
 }
 .pastel3 {
   color: black;
@@ -39,7 +106,6 @@ export default {
   font-family: "Georgia", "Times New Roman", Times, serif, serif;
   padding: 16px;
   border-style: solid;
-
   width: 140px;
 }
 .pastel4 {
@@ -57,11 +123,8 @@ export default {
 }
 
 .flex-container {
-  align-items:center;
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
 }
 
 a:link {
@@ -78,5 +141,12 @@ a:hover {
 
 a:active {
   text-decoration: underline;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr auto minmax(100px, 1fr) 1fr;
+  grid-template-rows: 100px repeat(3, auto) 100px;
+  grid-gap: 10px;
 }
 </style>
